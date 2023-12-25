ace20f118700eb5b0d86334fc053f9c454f2b26e
dev-rahul-0 <rs650417@gmail.com>
2023-12-25 20:41:13 +0530

    Function Created

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,12 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:projecknew/chat/chat_bubble.dart';
 import 'package:projecknew/chat/chat_service.dart';
 import 'package:projecknew/components/my_text_field.dart';
+
 class ChatPage extends StatefulWidget {
   final String receiverUserEmail;
   final String receverUserId;
-  const ChatPage({super.key,required this.receiverUserEmail,required this.receverUserId});
+
+  const ChatPage(
+      {super.key,
+      required this.receiverUserEmail,
+      required this.receverUserId});
 
   @override
   State<ChatPage> createState() => _ChatPageState();
@@ -28,64 +34,92 @@ class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.receiverUserEmail),),
+      appBar: AppBar(
+        title: Text(widget.receiverUserEmail),
+      ),
       body: Column(
         children: [
-          Expanded(child: _buildMessageList(),
+          Expanded(
+            child: _buildMessageList(),
           ),
           _buildMessageInput(),
-        ],
 
+          SizedBox(height: 25,)
+        ],
       ),
     );
   }
+
   Widget _buildMessageList() {
-    return StreamBuilder(stream: _chatService.getMessage(widget.receverUserId, _firebaseAuth.currentUser!.uid),
-        builder: (context, snapshot){
-      if (snapshot.hasError) {
-        return Text('Error${snapshot.error}');
-      }
-      if (snapshot.connectionState == ConnectionState.waiting) {
-        return const Text('Loading....');
-      }
-      return ListView(
-        children: snapshot.data!.docs
-        .map((document) => _buildMessageItem(document)).toList(),
-      );
+    return StreamBuilder(
+        stream: _chatService.getMessage(
+            widget.receverUserId, _firebaseAuth.currentUser!.uid),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error${snapshot.error}');
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Text('Loading....');
+          }
+          return ListView(
+            children: snapshot.data!.docs
+                .map((document) => _buildMessageItem(document))
+                .toList(),
+          );
         });
   }
-  Widget _buildMessageItem(DocumentSnapshot document){
+
+  Widget _buildMessageItem(DocumentSnapshot document) {
     Map<String, dynamic> data = document.data() as Map<String, dynamic>;
 
     var alignment = (data['senderId'] == _firebaseAuth.currentUser!.uid)
-    ? Alignment.centerRight
-    : Alignment.centerLeft;
+        ? Alignment.centerRight
+        : Alignment.centerLeft;
 
     return Container(
       alignment: alignment,
-      child: Column(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment:
+              (data['senderId'] == _firebaseAuth.currentUser!.uid)
+                  ? CrossAxisAlignment.end
+                  : CrossAxisAlignment.start,
+          mainAxisAlignment:
+              (data['senderId'] == _firebaseAuth.currentUser!.uid)
+                  ? MainAxisAlignment.end
+                  : MainAxisAlignment.start,
+          children: [
+            Text(data['senderEmail']),
+            SizedBox(height: 5,),
+            ChatBubble(message: data['message']),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMessageInput() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: Row(
         children: [
-          Text(data['senderEmail']),
-          Text(data['message']),
+          Expanded(
+            child: MyTextField(
+              controller: _messageController,
+              hintText: "Enter message",
+              obscureText: false,
+            ),
+          ),
+          IconButton(
+            onPressed: sendMessage,
+            icon: Icon(
+              Icons.arrow_upward,
+              size: 40,
+            ),
+          )
         ],
       ),
     );
-
   }
-
-
-
-  Widget _buildMessageInput() {
-    return Row(
-      children: [
-        Expanded(child: MyTextField(
-          controller: _messageController,
-          hintText: "Enter message",
-          obscureText: false,
-        ),),
-        IconButton(onPressed: sendMessage, icon: Icon(Icons.arrow_upward,size: 40,),)
-      ],
-    );
-  }
-
 }
