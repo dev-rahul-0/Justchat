aca0c77bd6b2a9a7b1be81c38d91636e0c24bf4f
dev-rahul-0 <rs650417@gmail.com>
2024-01-14 13:20:27 +0530

    UI update

---
+++ lib/chat/chat_bubble.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+class ChatBubble extends StatelessWidget {
+  final String message;
+  const ChatBubble({super.key,required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: Colors.blue
+      ),
+      child: Text(
+        message,
+        style: TextStyle(fontSize: 16, color: Colors.white),
+      ),
+    );
+  }
+}
